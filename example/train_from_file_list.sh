4d98afe24c89331db88b17fcc1595d26f4b23641
Hiroshi Noji <h.nouji@gmail.com>
2014-12-17 13:27:23 +0900

    Modify an example

--- example/train_from_file_list.sh
+++ example/train_from_file_list.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 ../build/src/topiclm_train --file=./nips.lst --format=1 --model=nips_model --num_topics=50 --burn-ins=100 --unk_handler=2 --unk_converter=1 --unk_stream_start=0
+
+echo 'A set of H units is used for the hidden layer .' | ../build/src/log_probability --model=nips_model/model/100.out
